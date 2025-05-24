55b0ba7f4fe44325674d610123d47ef5b25f64b1
GRPujol <grpujol@gmail.com>
2025-05-24 12:35:02 -0300

    1

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const [metodo, entidadEId, ...opciones] = process.argv.slice(2);
+
 const [entidad, id] = entidadEId.split('/');
+
 var options = { method: metodo }
+
 const nuevoProducto = {};
-if (metodo === 'POST') {
+
+if (metodo == 'POST') {
+    //solo si es un POST, se crea el producto
     nuevoProducto.id = 0;
     nuevoProducto.title = opciones[0];
     nuevoProducto.price = opciones[1];
     nuevoProducto.description = opciones[0];
     nuevoProducto.category = opciones[2];
     nuevoProducto.image = '';
+
     options.headers = { 'Content-Type': 'application/json' };
     options.body = JSON.stringify(nuevoProducto);
 }
-const comando = `${entidad}${id ? '/' + id : ''}`;
+
+var comando  = '';
+
+if(id!=undefined){
+    comando = `${entidad}/${id}`;
+}else{
+    comando = `${entidad}`;
+};
+
+console.log(`Comando: ${comando}`);
+
 fetch(`https://fakestoreapi.com/${comando}`, options)
     .then(response => {
         return response.json();
     })
     .then(data => {
-        console.dir(data, { depth: null, colors: true });
+        console.dir(data, { depth: null, colors: true }); //para que muestre todo si se usa users o cards
     });
